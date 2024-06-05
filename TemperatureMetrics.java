eaace7838c1000feea2c6b3b4b50b0ea690a6964
Azim Azizov <87415292+Azim-Azizov@users.noreply.github.com>
2024-06-05 10:28:02 +0400

    Update TemperatureMetrics.java

--- TemperatureMetrics.java
+++ TemperatureMetrics.java
@@ -1,51 +1,41 @@
-import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.util.concurrent.atomic.AtomicLong;
 
 public class TemperatureMetrics {
-//    private final String name;
-    private final AtomicLong quantity = new AtomicLong(1);
-    private BigDecimal min;
-    private BigDecimal max;
-    private BigDecimal total;
+    private int quantity = 1;
+    private float min;
+    private float max;
+    private long total;
 
-    public TemperatureMetrics(String name, String temperature) {
-//        this.name = name;
-        BigDecimal value = new BigDecimal(temperature);
-        this.min = value;
-        this.max = value;
-        this.total = value;
+    public TemperatureMetrics(float temperature) {
+        this.min = temperature;
+        this.max = temperature;
+        this.total = (long) (temperature * 10);
     }
 
-//    public String getName() {
-//        return name;
-//    }
-
-    public BigDecimal getMin() {
+    public float getMin() {
         return min;
     }
 
-    public BigDecimal getMax() {
+    public float getMax() {
         return max;
     }
 
-    public synchronized void add(String temperature) {
-        BigDecimal bigDecimal = new BigDecimal(temperature);
-        this.total = this.total.add(bigDecimal);
-        this.quantity.incrementAndGet();
-        if (bigDecimal.compareTo(this.max) > 0) {
-            this.max = bigDecimal;
-        } else if (bigDecimal.compareTo(this.min) < 0) {
-            this.min = bigDecimal;
+    public synchronized void add(float temperature) {
+        this.total += (long) (temperature * 10);
+        this.quantity += 1;
+        if (temperature > this.max) {
+            this.max = temperature;
+        } else if (temperature < this.min) {
+            this.min = temperature;
         }
     }
 
-    public BigDecimal getAverage() {
-        return this.total.divide(new BigDecimal(this.quantity.get()), 1, RoundingMode.HALF_UP);
+    public float getAverage() {
+        return this.total / (this.quantity * 10f);
     }
 
     @Override
     public String toString() {
-        return this.min.setScale(1, RoundingMode.HALF_UP) + "/" + this.getAverage().toString() + "/" + this.max.setScale(1, RoundingMode.HALF_UP);
+        return this.min + "/" + String.format("%.1f", this.getAverage()) + "/" + this.max;
     }
 }

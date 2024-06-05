d1ccc6cfe818ca2ac1dc52a2709c3008cf9be853
Azim Azizov <87415292+Azim-Azizov@users.noreply.github.com>
2024-06-05 10:26:39 +0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,21 +1,35 @@
-import java.io.*;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class Main {
-    private static final Integer THREADS = 20;
-    private static final Integer LINES = 102400000;
-    public static void main(String[] args) throws IOException {
-        Map<String, TemperatureMetrics> data = new ConcurrentHashMap<String, TemperatureMetrics>();
+    private static final Integer THREADS = 10;
+    private static final File file = new File("../1brc/measurements.txt");
+    private static final long FILESIZE = file.length();
+    private static final long CHUNKSIZE = FILESIZE / THREADS;
+
+    public static void main(String[] args) throws Exception {
+        Map<String, TemperatureMetrics> data = new ConcurrentHashMap<>();
         try (ExecutorService executorService = Executors.newFixedThreadPool(THREADS)) {
+            List<Future<Void>> futures = new ArrayList<>();
+            long start = 0;
             for (int i = 0; i < THREADS; i++) {
-                executorService.submit(new ReaderTask(i, i * LINES, LINES, data));
+                long end = (i == THREADS - 1) ? FILESIZE : start + CHUNKSIZE;
+                futures.add(executorService.submit(new ReaderTask(file, start, end, data)));
+                start = end;
             }
-        } catch (RuntimeException e) {
-            e.printStackTrace();
+
+            for (Future<Void> future : futures) {
+                future.get();
+            }
         }
-        System.out.println(data.toString());
+        Map<String, TemperatureMetrics> result = new TreeMap<>(data);
+        System.out.println(result);
     }
 }
